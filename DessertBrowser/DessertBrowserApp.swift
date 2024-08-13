ba752cbf8c239f8532cb24bf547e7825745b8bba
Joey Nash <jrnash621@gmail.com>
2024-08-12 20:34:23 -0400

    Initial Commit

---
+++ DessertBrowser/DessertBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertBrowserApp.swift
+//  DessertBrowser
+//
+//  Created by Joseph Nash on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
