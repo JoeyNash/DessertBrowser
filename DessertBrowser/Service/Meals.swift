b5f36b0a8dbec45e29c31acf1eee85d74fedf804
Joey Nash <jrnash621@gmail.com>
2024-08-14 09:38:08 -0400

    Fixed details decoding on unexpected values

--- DessertBrowser/Service/Meals.swift
+++ DessertBrowser/Service/Meals.swift
@@ -116,6 +116,58 @@ struct MealDetails: Codable {
     self.measure18 = measure18
     self.measure19 = measure19
     self.measure20 = measure20
+
+    // Setting ingredients/measures as array for easy display in SwiftUI
+    setIngredientMeasuresStrings()
+  }
+
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    id = try container.decode(String.self, forKey: .id)
+    name = try container.decode(String.self, forKey: .name)
+    thumbnailURL = try container.decode(String.self, forKey: .thumbnailURL)
+    instructions = try container.decode(String.self, forKey: .instructions)
+    ingredient1 = try container.decode(String?.self, forKey: .ingredient1)
+    ingredient2 = try container.decode(String?.self, forKey: .ingredient2)
+    ingredient3 = try container.decode(String?.self, forKey: .ingredient3)
+    ingredient4 = try container.decode(String?.self, forKey: .ingredient4)
+    ingredient5 = try container.decode(String?.self, forKey: .ingredient5)
+    ingredient6 = try container.decode(String?.self, forKey: .ingredient6)
+    ingredient7 = try container.decode(String?.self, forKey: .ingredient7)
+    ingredient8 = try container.decode(String?.self, forKey: .ingredient8)
+    ingredient9 = try container.decode(String?.self, forKey: .ingredient9)
+    ingredient10 = try container.decode(String?.self, forKey: .ingredient10)
+    ingredient11 = try container.decode(String?.self, forKey: .ingredient11)
+    ingredient12 = try container.decode(String?.self, forKey: .ingredient12)
+    ingredient13 = try container.decode(String?.self, forKey: .ingredient13)
+    ingredient14 = try container.decode(String?.self, forKey: .ingredient14)
+    ingredient15 = try container.decode(String?.self, forKey: .ingredient15)
+    ingredient16 = try container.decode(String?.self, forKey: .ingredient16)
+    ingredient17 = try container.decode(String?.self, forKey: .ingredient17)
+    ingredient18 = try container.decode(String?.self, forKey: .ingredient18)
+    ingredient19 = try container.decode(String?.self, forKey: .ingredient19)
+    ingredient20 = try container.decode(String?.self, forKey: .ingredient20)
+    measure1 = try container.decode(String?.self, forKey: .measure1)
+    measure2 = try container.decode(String?.self, forKey: .measure2)
+    measure3 = try container.decode(String?.self, forKey: .measure3)
+    measure4 = try container.decode(String?.self, forKey: .measure4)
+    measure5 = try container.decode(String?.self, forKey: .measure5)
+    measure6 = try container.decode(String?.self, forKey: .measure6)
+    measure7 = try container.decode(String?.self, forKey: .measure7)
+    measure8 = try container.decode(String?.self, forKey: .measure8)
+    measure9 = try container.decode(String?.self, forKey: .measure9)
+    measure10 = try container.decode(String?.self, forKey: .measure10)
+    measure11 = try container.decode(String?.self, forKey: .measure11)
+    measure12 = try container.decode(String?.self, forKey: .measure12)
+    measure13 = try container.decode(String?.self, forKey: .measure13)
+    measure14 = try container.decode(String?.self, forKey: .measure14)
+    measure15 = try container.decode(String?.self, forKey: .measure15)
+    measure16 = try container.decode(String?.self, forKey: .measure16)
+    measure17 = try container.decode(String?.self, forKey: .measure17)
+    measure18 = try container.decode(String?.self, forKey: .measure18)
+    measure19 = try container.decode(String?.self, forKey: .measure19)
+    measure20 = try container.decode(String?.self, forKey: .measure20)
+    setIngredientMeasuresStrings()
   }
   
   // Base details
@@ -338,16 +390,18 @@ struct MealDetails: Codable {
     return measures
   }
 
-  /// Combination of measures and ingredients
-  lazy var ingredientMeasuresStrings: [String] = {
-    ingredients.enumerated().compactMap {
+  private mutating func setIngredientMeasuresStrings() {
+    ingredientMeasuresStrings = ingredients.enumerated().compactMap {
       // Confirm we have a matching ingredient/measure combo
       guard measures.count > $0.offset else {
         return nil
       }
-      return $0.element + " " + measures[$0.offset]
+      return $0.element.capitalized + " " + measures[$0.offset]
     }
-  }()
+  }
+
+  /// Combination of measures and ingredients for SwiftUI display
+  var ingredientMeasuresStrings: [String] = []
   
   enum CodingKeys: String, CodingKey {
     case name = "strMeal"
